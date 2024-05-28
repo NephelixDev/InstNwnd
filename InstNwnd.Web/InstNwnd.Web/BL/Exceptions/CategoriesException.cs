8130af41a65909af94ad0e79f4c84a334e5a2602
NephelixDev <nephelix.dev@gmail.com>
2024-05-28 16:04:19 -0400

    Carpetas vacias corregidas

---
+++ InstNwnd.Web/InstNwnd.Web/BL/Exceptions/CategoriesException.cs
@@ -0,0 +1,9 @@
+﻿namespace InstNwnd.Web.BL.Exceptions
+{
+    public class CategoriesException : Exception
+    {
+        public CategoriesException(string message) : base(message)
+        {
+        }
+    }
+}
