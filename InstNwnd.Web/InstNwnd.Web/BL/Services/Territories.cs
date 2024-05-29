26084cabe892e21ac5f782c1179ac220d413da7d
kelobel <165127136+kelobel@users.noreply.github.com>
2024-05-28 20:06:46 -0400

    keobel tabla completa

---
+++ InstNwnd.Web/InstNwnd.Web/BL/Services/Territories.cs
@@ -0,0 +1,8 @@
+﻿using InstNwnd.Web.BL.Interfaces;
+
+namespace InstNwnd.Web.BL.Services
+{
+    public class Territories : ITerritoriesService
+    {
+    }
+}
---
+++ InstNwnd.Web/InstNwnd.Web/Data/Entities/Territories.cs
@@ -0,0 +1,8 @@
+﻿namespace InstNwnd.Web.Data.Entities
+{
+    public class Territories
+    {
+        public string TerritoryDescription { get; set; }
+        public int RegionId { get; set; }
+    }
+}
